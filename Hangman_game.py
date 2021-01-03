d4c8e570462e2800fb8f4fecd717538215de2e12
Washim Akram Khan <61159573+wak75@users.noreply.github.com>
2021-01-03 23:46:03 +0530

    Update Hangman_game.py

--- Hangman_game.py
+++ Hangman_game.py
@@ -63,25 +63,13 @@ chosen_word = random.choice(word_list)
 
 display=[]
 
-#Testing code
+
 print(f'Pssst, the available options are {word_list}. \nYour job is to choose the correct word, before you ran out of lives')
 
-#TODO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+
 for n in range(len(chosen_word)):
   display.append("_")
 
-
-
-#TODO-2: - Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-# for letter in chosen_word:
-#     if letter == guess:
-#         display[n]=guess
-#     else:
-#         print("Wrong")
 
 lives=7
 
@@ -103,6 +91,3 @@ while "_" in display!=False:
       exit()
     elif "_" not in display:
       print("congrats!! you win")
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
-
